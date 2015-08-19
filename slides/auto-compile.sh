96ed8c0db6a389ab189ecdb135b2b2606d93797a
auzias <github@auzias.net>
2015-08-19 16:23:07 +0200

    IPv6 slides update (and appended)

--- slides/auto-compile.sh
+++ slides/auto-compile.sh
@@ -2,5 +2,6 @@
 
 while inotifywait -e modify ./*tex; do
   pdflatex -output-format pdf -jobname=slides all
+  pdflatex -output-format pdf -jobname=slides all
   rm *aux *log *out *toc *bib *tex\~ *.nav *bbl *blg *snm
 done
