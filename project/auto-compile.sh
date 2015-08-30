dc06382a3e7299808435103a47910672c936c7c3
auzias <github@auzias.net>
2015-08-30 13:17:55 +0200

    filename corrected

--- project/auto-compile.sh
+++ project/auto-compile.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 while inotifywait -e modify ./*tex; do
-  pdflatex -output-format pdf tutorials
-  pdflatex -output-format pdf practice
+  pdflatex -output-format pdf project-expectations
   rm *aux *log *out *toc *bib
 done
